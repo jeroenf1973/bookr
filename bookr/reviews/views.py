2f852eba2c2e913f354c911ec4d488e600f84842
Findhammer <jeroen.findhammer@radboudumc.nl>
2022-04-25 17:18:50 +0200

    pag 45

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,3 +1,11 @@
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse 
+
+def index(request):
+    name = request.GET.get("name") or "world"
+    return HttpResponse(f"Hello {name}!")
+
+
