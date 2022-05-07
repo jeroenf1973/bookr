78a9f5c1035b7dedad8e190ae2eee2240d1869c5
JeFi <jeroenf1973@hotmail.com>
2022-05-07 18:07:03 +0200

    End of introduction, chapter 1

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -2,10 +2,19 @@ from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse 
+#from django.http import HttpResponse 
+# Geen eigen http resonse opbject meer, deze gaan we maken via de render functie
+from django.shortcuts import render
 
 def index(request):
-    name = request.GET.get("name") or "world"
-    return HttpResponse(f"Hello {name}!")
+    #name = request.GET.get("name") or "world"
+    #return HttpResponse(f"Hello {name}!")
+    name = "Jeroen"
+    #return render(request, "base.html", {"name": name})
+    return render(request, "base.html")
+
+def book_search(request):
+    search_text = request.GET.get("search")
+    return render(request, "book-search.html", {"search_text": search_text})
 
 
